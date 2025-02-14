ad428cfe6bc81258c6136d11000a411c20d5fc72
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 07:31:30 +0300

    Bump version to 1.4.6 and update VerhaltReference type definition for improved structure

--- src/lib/verhaltReference.ts
+++ src/lib/verhaltReference.ts
@@ -1,9 +1,7 @@
 import { VerhaltObjectModel } from "./verhaltModel";
 
-export type VerhaltReference = VerhaltReferenceEntry[]
+export type VerhaltReference = [VerhaltReferenceValue, VerhaltObjectModel]
 
-export type VerhaltReferenceEntry = [VerhaltReferenceEntryValue, VerhaltObjectModel];
-
-export type VerhaltReferenceEntryValue = string | number;
+export type VerhaltReferenceValue = string | number;
 
 export type VerhaltReferenceMatch = "target" | "parent" | "list" | "list+" | "source";
