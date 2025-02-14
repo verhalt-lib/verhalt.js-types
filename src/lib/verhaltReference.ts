61e9cb837cf36cfdce419ed90436844af1aa91c6
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 05:37:32 +0300

    Bump version to 1.4.3 and add VerhaltReferenceType for enhanced type definitions

--- src/lib/verhaltReference.ts
+++ src/lib/verhaltReference.ts
@@ -3,3 +3,5 @@ import { VerhaltObjectModel } from "./verhaltModel";
 export type VerhaltReference = VerhaltReferenceEntry[]
 
 export type VerhaltReferenceEntry = [string, VerhaltObjectModel]
+
+export type VerhaltReferenceType = "target" | "parent" | "full";
